59f15f96bd2b2b936948beeae7c599c0c3757a59
Saad Ali Shujaat <saadali0075368@gamil.com>
2021-10-28 23:59:18 -0700

    FCM

--- app/src/main/java/com/pandalibs/pandafcm/MainActivity.kt
+++ app/src/main/java/com/pandalibs/pandafcm/MainActivity.kt
@@ -2,12 +2,15 @@ package com.pandalibs.pandafcm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.FirebaseApp
 import com.pandalibs.fcmlib.PandaFCM
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this)
 
         PandaFCM.setupFCM(this, "YourTopicName")
 
